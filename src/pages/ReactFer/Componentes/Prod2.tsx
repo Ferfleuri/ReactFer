a36a71b91b0accb10a502a9033b942cd9c424aff
Ferfleuri <fernandofleuri1@gmail.com>
2023-09-22 12:23:34 -0300

    atualizado

---
+++ src/pages/ReactFer/Componentes/Prod2.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Image, Text, StyleSheet, Dimensions, View } from "react-native";
+
+import produto2 from "../../../../assets/produto.jpg";
+import logo from "../../../../assets/logo.png";
+
+const width = Dimensions.get("screen").width;
+
+export default function Prod1() {
+  return (
+    <>
+      <Image source={produto2} style={personalizar.produto} />
+      <View style={personalizar.skincare}>
+        <View style={personalizar.logo}>
+          <Image style={personalizar.imagemLogo} source={logo} />
+          <Text style={personalizar.nomeLogo}>Conjunto de Skincare</Text>
+        </View>
+        <Text style={personalizar.descricao}>
+          Conjunto de Skincare Ordinary: Sérum ; Gel de limpeza ; Hidratante
+          Facial ; Protetor Solar .
+        </Text>
+        <Text style={personalizar.preco}>R$210.00</Text>
+      </View>
+    </>
+  );
+}
+
+const personalizar = StyleSheet.create({
+  produto: {
+    width: "100%",
+    height: (700 / 920) * width,
+  },
+  skincare: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  nomeLogo: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12,
+  },
+  imagemLogo: {
+    width: 32,
+    height: 32,
+  },
+  logo: {
+    flexDirection: "row",
+    paddingVertical: 12,
+  },
+  descricao: {
+    color: "#A3A3A3",
+    fontStyle: "italic",
+  },
+  preco: {
+    fontSize: 26,
+    lineHeight: 42,
+    color: "#556B2F",
+    marginTop: 8,
+  },
+});
--- src/pages/ReactFer/index.tsx
+++ src/pages/ReactFer/index.tsx
@@ -1,71 +1,16 @@
-import React from 'react';
-import { StyleSheet, Image, Dimensions, Text, View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
-import produto from '../../../assets/produto.jpg'
-import produto2 from '../../../assets/produto2.jpg'
-import logo from '../../../assets/logo.png'
-
-const width = Dimensions.get('screen').width;
+import Prod1 from "./Componentes/Prod1";
+import Prod2 from "./Componentes/Prod2";
 
 export default function ReactFer() {
-  return <>
-  <Image source={produto} style={personalizar.produto}/>
-    <View style={personalizar.skincare}>
-      <View style={personalizar.logo}>
-        <Image style={personalizar.imagemLogo} source={logo}/>
-        <Text style={personalizar.nomeLogo}>Conjunto de Skincare</Text>
+  return (
+    <>
+      <View>
+        <Prod1 />
+        <Prod2 />
       </View>
-      <Text style={personalizar.descricao}>Conjunto de Skincare Ordinary:
-      Tônico Facial ; Gel de limpeza ; Sabonete Facial ; Sérum .
-      </Text>
-      <Text style={personalizar.preco}>R$180.00</Text>
-    </View>
-
-    <Image source={produto2} style={personalizar.produto}/>
-    <View style={personalizar.skincare}>
-      <View style={personalizar.logo}>
-        <Image style={personalizar.imagemLogo} source={logo}/>
-        <Text style={personalizar.nomeLogo}>Conjunto de Skincare</Text>
-      </View>
-      <Text style={personalizar.descricao}>Conjunto de Skincare Ordinary:
-      Sérum ; Gel de limpeza ; Hidratante Facial ; Protetor Solar .
-      </Text>
-      <Text style={personalizar.preco}>R$210.00</Text>
-    </View>
-
-  </>
+    </>
+  );
 }
-
-const personalizar = StyleSheet.create({
-  produto: {
-    width: "100%",
-    height: 700 / 920 * width
-  },
-  skincare: {
-    paddingVertical: 8,
-    paddingHorizontal: 16
-  },
-  nomeLogo: {
-    fontSize: 16,
-    lineHeight: 26,
-    marginLeft: 12
-  },
-  imagemLogo: {
-    width: 32,
-    height: 32
-  },
-  logo: {
-    flexDirection: "row",
-    paddingVertical: 12
-  },
-  descricao: {
-    color: "#A3A3A3",
-    fontStyle: "italic"
-  },
-  preco:{
-    fontSize: 26,
-    lineHeight: 42,
-    color: "#556B2F",
-    marginTop: 8
-  }
-})
